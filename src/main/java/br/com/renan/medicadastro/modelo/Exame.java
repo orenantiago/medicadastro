e4f1d5cca54ef53d355e44e19e733465ead9701d
Renan Silva <renan.tssilva@gmail.com>
2017-07-31 20:07:12 -0300

    add Exame model

---
+++ src/main/java/br/com/renan/medicadastro/modelo/Exame.java
@@ -0,0 +1,10 @@
+package br.com.renan.medicadastro.modelo;
+
+import java.util.Calendar;
+
+public class Exame {
+	private long id;
+	private String nome;
+	private String descricao;
+	private Calendar data;
+}
