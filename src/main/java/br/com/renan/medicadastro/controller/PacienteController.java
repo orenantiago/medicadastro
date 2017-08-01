036746f68d7c27311e55275de4e462d1b8953f8f
Renan Silva <renan.tssilva@gmail.com>
2017-07-31 23:01:55 -0300

    add view formulario-paciente.jsp and the method addPaciente to class PacienteController

--- src/main/java/br/com/renan/medicadastro/controller/PacienteController.java
+++ src/main/java/br/com/renan/medicadastro/controller/PacienteController.java
@@ -1,8 +1,13 @@
 package br.com.renan.medicadastro.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PacienteController {
 	
+	@RequestMapping("/cadastroPaciente")
+	public String getFormularioPaciente() {
+		return "/paciente/formulario-paciente";
+	}
 }
