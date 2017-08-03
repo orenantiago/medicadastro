3ade4a9ab8655a696e74f2a1800a5ba8168ba372
Renan Silva <renan.tssilva@gmail.com>
2017-08-03 17:29:34 -0300

    make PacienteDao return pacient by id

--- src/main/java/br/com/renan/medicadastro/dao/PacienteDao.java
+++ src/main/java/br/com/renan/medicadastro/dao/PacienteDao.java
@@ -20,62 +20,62 @@ public class PacienteDao {
 	@Autowired
 	public PacienteDao() throws SQLException {
 		this.connection = new ConnectionFactory().getConnection();
-		//this.connection = dataSource.getConnection();
+		// this.connection = dataSource.getConnection();
 	}
-	
+
 	public void adiciona(Paciente paciente) {
-		String sql = "insert into paciente(nome, idade, sexo) "
-				+ "values(?,?,?)";
-				
+		String sql = "insert into paciente(nome, idade, sexo) " + "values(?,?,?)";
+
 		try {
 			PreparedStatement stmt = connection.prepareStatement(sql);
-			
+
 			stmt.setString(1, paciente.getNome());
 			stmt.setInt(2, paciente.getIdade());
 			stmt.setString(3, paciente.getSexo());
-			
+
 			stmt.execute();
 			stmt.close();
-			
+
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
-	public List<Paciente> lista () throws SQLException {
+
+	public List<Paciente> lista() throws SQLException {
 		List<Paciente> lista = new ArrayList<Paciente>();
 		PreparedStatement stmt = this.connection.prepareStatement("select * from paciente");
 		ResultSet rs = stmt.executeQuery();
-		while(rs.next()) {
+		while (rs.next()) {
 			Paciente paciente = new Paciente();
 			paciente.setId(rs.getInt("id"));
 			paciente.setNome(rs.getString("nome"));
 			paciente.setIdade(rs.getInt("idade"));
 			paciente.setSexo(rs.getString("sexo"));
-			
+
 			lista.add(paciente);
 		}
-		
+
 		stmt.close();
 		rs.close();
 		return lista;
 	}
-	
+
 	public Paciente getPaciente(long id) throws SQLException {
 		PreparedStatement stmt = this.connection.prepareStatement("select * from paciente where id=?");
 		stmt.setString(1, String.valueOf(id));
-		ResultSet rs = stmt.executeQuery();
-		
 		Paciente paciente = new Paciente();
-		paciente.setId(rs.getLong("id"));
-		paciente.setNome(rs.getString("nome"));
-		paciente.setIdade(rs.getInt("idade"));
-		paciente.setSexo(rs.getString("sexo"));
-		
-		stmt.close();
-		rs.close();
-		
+
+		ResultSet rs = stmt.executeQuery();
+		if (rs.next()) {
+			paciente.setId(rs.getLong("id"));
+			paciente.setNome(rs.getString("nome"));
+			System.out.println(rs.getString("nome"));
+			paciente.setIdade(rs.getInt("idade"));
+			paciente.setSexo(rs.getString("sexo"));
+			stmt.close();
+			rs.close();
+		}
 		return paciente;
-		
+
 	}
 }
