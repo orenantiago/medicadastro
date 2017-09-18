72777e4bf0c232ed86771450b950b473310f7218
Renan Silva <renan.tssilva@gmail.com>
2017-09-18 17:39:37 -0300

    create add exame and add paciente functionality

--- webapp/js/app.js
+++ webapp/js/app.js
@@ -6,14 +6,14 @@ angular
 	'pacients'
 ]);
 
-angular.module('app',[]).controller('meu.controller',[
+angular.module('app').controller('meu.controller',[
         '$scope'
         ,'$http'
         ,function meuController($scope, $http){ console.info("Olá mundo") }
         ] );
 
 angular
-    .module('exams')
+    .module('exams', [])
     .controller('listExamsController', listExamsController);
 
 listExamsController.$inject = ['$http'];
@@ -25,10 +25,36 @@ function listExamsController($http) {
 
 	});
 }
+//CHECK IF TRUE
+angular
+	.module('exams')
+	.controller('delExamController', function modify($http, $scope){
+		var id = $scope.exame.id;
+		$scope.delete = function() {
+			console.log(id)
+			$http.delete('http://localhost:8080/exames/' + id).then(function(response) {
+				console.log(response)
+				//self.pacientes = response.data;
+
+			});
+		};
+	});
+angular
+	.module('exams')
+	.controller('addExamController', function add($http, $scope){
+
+		$scope.add = function() {
+			$http.post('http://localhost:8080/exames/', $scope.exame).then(function(response) {
+				console.log(response)
+				//self.pacientes = response.data;
+
+			});
+		};
+	});
 
 angular
   .module('pacients', [])
-  .controller('listPacientsController', function listPacientsController($http) {
+  .controller('listPacientsController', function listPacientsController($http, $scope) {
 		var self = this;
 		$http.get('http://localhost:8080/pacientes').then(function(response) {
 			console.log(response)
@@ -37,11 +63,44 @@ angular
 		});
 	});
 
+	angular
+	  .module('pacients', [])
+	  .controller('listPacientsController', function form($http, $scope) {
+			var self = this;
+			$http.get('http://localhost:8080/pacientes').then(function(response) {
+				console.log(response)
+				self.pacientes = response.data;
+
+			});
+		});
+
 //TODO
 
-// angular
-// 	.module('pacients')
-// 	.controller('deletePacientController', function delPacient($http, $scope){
-// 		var id = $scope.paciente.id;
-// 		console.log(id);
-// 	});
+angular
+	.module('pacients')
+	.controller('delPacientController', function del($http, $scope){
+		//var id = $scope.paciente.id;
+		$scope.delete = function() {
+			console.log(id)
+			$http.delete('http://localhost:8080/pacientes/' + id).then(function(response) {
+				console.log(response)
+			})
+			delete $scope.paciente
+		};
+		// $scope.show = function() {
+		//
+		// }
+	});
+
+	angular
+		.module('pacients')
+		.controller('addPacientController', function add($http, $scope){
+
+			$scope.add = function() {
+				$http.post('http://localhost:8080/pacientes/', $scope.paciente).then(function(response) {
+					console.log(response)
+					//self.pacientes = response.data;
+
+				});
+			};
+		});
