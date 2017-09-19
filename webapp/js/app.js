fa7c5087e01d0fbf507b7d98927b048d4e1f91f7
Renan Silva <renan.tssilva@gmail.com>
2017-09-19 15:05:33 -0300

    make 'alter' button in ng-repeat work

--- webapp/js/app.js
+++ webapp/js/app.js
@@ -1,10 +1,55 @@
 
 'use strict';
 angular
-.module('app', [
-	'exams',
-	'pacients'
-]);
+.module('app', [])
+.controller('controller', function controller($http, $scope){
+	var self = this;
+	$http.get('http://localhost:8080/exames').then(function(response) {
+		console.log(response)
+		self.exames = response.data;
+	});
+
+	$http.get('http://localhost:8080/pacientes').then(function(response) {
+		console.log(response)
+		self.pacientes = response.data;
+	});
+
+	$scope.deleteExam = function(exame) {
+		console.log(exame.nome)
+		$http.delete('http://localhost:8080/exames/' + exame.id).then(function(response) {
+			console.log(response)
+		})
+	}
+
+	$scope.addExam = function(exame) {
+		console.log(exame.nome)
+		$http.post('http://localhost:8080/exames/', exame).then(function(response) {
+			console.log(response)
+		})
+	}
+
+
+	$scope.addPacient = function(paciente) {
+		console.log(paciente.nome)
+		$http.post('http://localhost:8080/pacientes/', paciente).then(function(response) {
+			console.log(response)
+		});
+	};
+
+	$scope.deletePacient = function(paciente) {
+		console.log(paciente.nome)
+		$http.delete('http://localhost:8080/pacientes/' + paciente.id).then(function(response) {
+			console.log(response)
+		})
+	};
+
+});
+
+angular.module('app').controller('meu.controller',[
+        '$scope'
+        ,'$http'
+        ,function meuController($scope, $http){ console.info("Olá mundo") }
+        ] );
 
 angular
     .module('exams', [])
